5f49ebd0582254603fe38abd58b8e14a5a426e98
Carl Bennett <carl@carlbennett.me>
2020-10-19 19:04:17 -0500

    Allow client to send mismatch token in SID_PING

--- src/Atlasd/Battlenet/Protocols/Game/Messages/SID_PING.cs
+++ src/Atlasd/Battlenet/Protocols/Game/Messages/SID_PING.cs
@@ -39,19 +39,21 @@ namespace Atlasd.Battlenet.Protocols.Game.Messages
 
             var token = (UInt32)((Buffer[3] << 24) + (Buffer[2] << 16) + (Buffer[1] << 8) + Buffer[0]);
 
-            if (token != context.Client.GameState.PingToken)
-                throw new GameProtocolViolationException(context.Client, "SID_PING token mismatch with server [Token: 0x" + token.ToString("X8") + "] [Server: 0x" + context.Client.GameState.PingToken.ToString("X8") + "]");
-
-            // Refresh the ping token that has now been used:
             lock (context.Client.GameState)
             {
+                if (token != context.Client.GameState.PingToken)
+                {
+                    Logging.WriteLine(Logging.LogLevel.Warning, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"SID_PING token mismatch with server [Token: 0x{token:X8}] [Server: 0x{context.Client.GameState.PingToken:X8}]");
+                }
+
+                // Refresh the ping token that has now been used:
                 context.Client.GameState.PingToken = (uint)new Random().Next(0, 0x7FFFFFFF);
             }
 
             var delta = DateTime.Now - context.Client.GameState.PingDelta;
             context.Client.GameState.Ping = (int)Math.Round(delta.TotalMilliseconds);
 
-            Logging.WriteLine(Logging.LogLevel.Debug, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, "[" + Common.DirectionToString(context.Direction) + "] SID_PING Token: [0x" + token.ToString("X8") + "] Ping: " + context.Client.GameState.Ping + "ms");
+            Logging.WriteLine(Logging.LogLevel.Info, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"Ping: {context.Client.GameState.Ping}ms");
 
             if (context.Client.GameState.ActiveChannel != null)
                 context.Client.GameState.ActiveChannel.UpdateUser(context.Client.GameState);
