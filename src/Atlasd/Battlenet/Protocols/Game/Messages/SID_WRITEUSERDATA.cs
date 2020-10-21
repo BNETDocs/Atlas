a2051981e265dafa5f03ab9acb4d40e7b79c97ec
Carl Bennett <carl@carlbennett.me>
2020-10-21 04:17:08 -0500

    Implement SID_WRITEUSERDATA

---
+++ src/Atlasd/Battlenet/Protocols/Game/Messages/SID_WRITEUSERDATA.cs
@@ -0,0 +1,101 @@
+﻿using Atlasd.Battlenet.Exceptions;
+using Atlasd.Daemon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Atlasd.Battlenet.Protocols.Game.Messages
+{
+    class SID_WRITEUSERDATA : Message
+    {
+        public SID_WRITEUSERDATA()
+        {
+            Id = (byte)MessageIds.SID_WRITEUSERDATA;
+            Buffer = new byte[0];
+        }
+
+        public SID_WRITEUSERDATA(byte[] buffer)
+        {
+            Id = (byte)MessageIds.SID_WRITEUSERDATA;
+            Buffer = buffer;
+        }
+
+        public override bool Invoke(MessageContext context)
+        {
+            Logging.WriteLine(Logging.LogLevel.Debug, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"[{Common.DirectionToString(context.Direction)}] SID_WRITEUSERDATA ({4 + Buffer.Length} bytes)");
+
+            if (context.Direction != MessageDirection.ClientToServer)
+                throw new GameProtocolViolationException(context.Client, "SID_WRITEUSERDATA must be sent from client to server");
+
+            if (Buffer.Length < 8)
+                throw new GameProtocolViolationException(context.Client, "SID_WRITEUSERDATA buffer must be at least 8 bytes");
+
+            var m = new MemoryStream(Buffer);
+            var r = new BinaryReader(m);
+
+            var numAccounts = r.ReadUInt32();
+            var numKeys = r.ReadUInt32();
+            var accounts = new List<string>();
+            var keys = new List<string>();
+            var values = new List<string>();
+
+            while (accounts.Count < numAccounts)
+            {
+                accounts.Add(r.ReadString());
+            }
+
+            while (keys.Count < numKeys)
+            {
+                keys.Add(r.ReadString());
+            }
+
+            while (values.Count < numKeys)
+            {
+                values.Add(r.ReadString());
+            }
+
+            r.Close();
+            m.Close();
+
+            var hasSudoPrivs = context.Client.GameState.ChannelFlags.HasFlag(Account.Flags.Admin) ||
+                context.Client.GameState.ChannelFlags.HasFlag(Account.Flags.Employee);
+
+            foreach (var accountName in accounts)
+            {
+                Battlenet.Common.AccountsDb.TryGetValue(accountName, out var account);
+
+                if (account == null)
+                {
+                    Logging.WriteLine(Logging.LogLevel.Warning, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, "Client attempted to write userdata for an account that does not exist");
+                    return false;
+                }
+
+                for (var i = 0; i < numKeys; i++)
+                {
+                    var key = keys[i];
+                    var value = values[i];
+
+                    if (!account.Get(key, out var dynvalue))
+                    {
+                        // skip this key, we do not write foreign keys
+                        continue;
+                    }
+                    var kv = dynvalue as AccountKeyValue;
+
+                    if (!(kv.Writable == AccountKeyValue.WriteLevel.Any ||
+                        (kv.Writable == AccountKeyValue.WriteLevel.Owner && (hasSudoPrivs || context.Client.GameState.ActiveAccount == account))))
+                    {
+                        Logging.WriteLine(Logging.LogLevel.Warning, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"Client attempted to write userdata to account [{accountName}] key [{key}] but they have no privilege to do so [hasSudoPrivs: {hasSudoPrivs}]");
+                        return false;
+                    }
+
+                    Logging.WriteLine(Logging.LogLevel.Info, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"Client wrote userdata to account [{accountName}] key [{key}] with [hasSudoPrivs: {hasSudoPrivs}]");
+                    kv.Value = value;
+                }
+            }
+
+            return true;
+        }
+    }
+}
