cf2f7e737a7af5a722f4540aafa18735eca0f27f
Carl Bennett <carl@carlbennett.me>
2020-10-31 17:36:15 -0500

    Add BNFTP File class

---
+++ src/Atlasd/Battlenet/Protocols/BNFTP/File.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Atlasd.Battlenet.Protocols.BNFTP
+{
+    class File
+    {
+        public bool Exists { get => new FileInfo(Name).Exists; }
+        public string Name { get; private set; }
+        public DateTime LastModified { get => System.IO.File.GetLastAccessTime(Name); }
+        public long Length { get => new FileInfo(Name).Length; }
+
+        public File(string filename)
+        {
+            Name = filename;
+        }
+
+        public StreamReader GetStream()
+        {
+            return new StreamReader(Name);
+        }
+    }
+}
