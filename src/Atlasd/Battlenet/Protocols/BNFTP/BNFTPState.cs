94fdac9d6f4b85c4397140c8d726190c8b186012
Carl Bennett <carl@carlbennett.me>
2020-11-01 01:10:47 -0600

    Actually add the class

---
+++ src/Atlasd/Battlenet/Protocols/BNFTP/BNFTPState.cs
@@ -0,0 +1,28 @@
+﻿using Atlasd.Battlenet.Protocols.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlasd.Battlenet.Protocols.BNFTP
+{
+    struct BNFTPState
+    {
+        public ClientState Client;
+
+        // Version 1
+        public UInt16 RequestLength;
+        public UInt16 ProtocolVersion;
+        public Platform.PlatformCode PlatformId;
+        public Product.ProductCode ProductId;
+        public UInt32 AdId;
+        public UInt32 AdFileExtension;
+        public UInt32 FileStartPosition;
+        public UInt64 FileTime;
+        public byte[] FileName;
+
+        // Version 2
+        public UInt32 ServerToken;
+        public UInt32 ClientToken;
+        public GameKey GameKey;
+    }
+}
