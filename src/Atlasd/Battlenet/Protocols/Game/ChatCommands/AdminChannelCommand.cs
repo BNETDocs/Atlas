b97a996915b591c609540940d132351eca6f7904
Caaaaarrrrlll <carl@bnetdocs.org>
2021-09-09 18:32:57 -0500

    Add admin channel commands

--- src/Atlasd/Battlenet/Protocols/Game/ChatCommands/AdminChannelCommand.cs
+++ src/Atlasd/Battlenet/Protocols/Game/ChatCommands/AdminChannelCommand.cs
@@ -15,15 +15,65 @@ namespace Atlasd.Battlenet.Protocols.Game.ChatCommands
 
         public override void Invoke(ChatCommandContext context)
         {
-            var r = "TODO";
+            var subcommand = Arguments.Count > 0 ? Arguments[0] : string.Empty;
+            if (!string.IsNullOrEmpty(subcommand))
+            {
+                Arguments.RemoveAt(0);
+            }
+
+            var eventId = ChatEvent.EventIds.EID_ERROR;
+            string reply = Resources.InvalidAdminCommand;
+            var channel = context.GameState.ActiveChannel;
+
+            if (channel != null)
+            {
+                switch (subcommand.ToLower())
+                {
+                    case "rename":
+                    case "name":
+                        {
+                            var newName = string.Join(" ", Arguments);
+                            if (!string.IsNullOrEmpty(newName))
+                            {
+                                reply = string.Empty;
+                                channel.SetName(newName);
+                            }
+                            break;
+                        }
+                    case "maxusers":
+                    case "maxuser":
+                        {
+                            if (Arguments.Count < 1)
+                            {
+                                eventId = ChatEvent.EventIds.EID_ERROR;
+                                reply = Resources.InvalidAdminCommand;
+                            }
+                            else
+                            {
+                                int.TryParse(Arguments[0], out var maxUsers);
+                                reply = string.Empty;
+                                channel.SetMaxUsers(maxUsers);
+                            }
+                            break;
+                        }
+                    case "topic":
+                        {
+                            reply = string.Empty;
+                            channel.SetTopic(string.Join(" ", Arguments));
+                            break;
+                        }
+                }
+            }
+
+            if (string.IsNullOrEmpty(reply)) return;
 
             foreach (var kv in context.Environment)
             {
-                r = r.Replace("{" + kv.Key + "}", kv.Value);
+                reply = reply.Replace("{" + kv.Key + "}", kv.Value);
             }
 
-            foreach (var line in r.Split(Battlenet.Common.NewLine))
-                new ChatEvent(ChatEvent.EventIds.EID_ERROR, context.GameState.ChannelFlags, context.GameState.Ping, context.GameState.OnlineName, line).WriteTo(context.GameState.Client);
+            foreach (var line in reply.Split(Battlenet.Common.NewLine))
+                new ChatEvent(eventId, context.GameState.ChannelFlags, context.GameState.Ping, context.GameState.OnlineName, line).WriteTo(context.GameState.Client);
         }
     }
 }
