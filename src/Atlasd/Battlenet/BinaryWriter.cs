c39ab42736da6d0a08687e4024bdbb2305b11cc0
Caaaaarrrrlll <carl@bnetdocs.org>
2023-01-25 04:06:51 -0600

    Make BinaryWriter thread-safe

--- src/Atlasd/Battlenet/BinaryWriter.cs
+++ src/Atlasd/Battlenet/BinaryWriter.cs
@@ -5,20 +5,28 @@ namespace Atlasd.Battlenet
 {
     class BinaryWriter : System.IO.BinaryWriter
     {
+        private readonly object _lock = new object();
+
         public BinaryWriter(Stream output) : base(output, Encoding.UTF8) { }
         public BinaryWriter(Stream output, Encoding encoding) : base(output, encoding) { }
         public BinaryWriter(Stream output, Encoding encoding, bool leaveOpen) : base(output, encoding, leaveOpen) { }
 
         public override void Write(string value)
         {
-            if (value != null) Write(Encoding.UTF8.GetBytes(value));
-            Write((byte)0); // null-terminator
+            lock (_lock)
+            {
+                if (value != null) Write(Encoding.UTF8.GetBytes(value));
+                Write((byte)0); // null-terminator
+            }
         }
 
         public void WriteByteString(byte[] value)
         {
-            if (value != null) Write(value);
-            Write((byte)0); // null-terminator
+            lock (_lock)
+            {
+                if (value != null) Write(value);
+                Write((byte)0); // null-terminator
+            }
         }
     }
 }
