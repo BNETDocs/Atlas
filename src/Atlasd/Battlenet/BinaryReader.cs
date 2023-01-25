9976601551b5002f88facd172d3701b34dbf0d7e
Caaaaarrrrlll <carl@bnetdocs.org>
2023-01-25 04:01:40 -0600

    Make BinaryReader thread-safe

--- src/Atlasd/Battlenet/BinaryReader.cs
+++ src/Atlasd/Battlenet/BinaryReader.cs
@@ -5,40 +5,51 @@ namespace Atlasd.Battlenet
 {
     class BinaryReader : System.IO.BinaryReader
     {
+        private readonly object _lock = new object();
+
         public BinaryReader(Stream input) : base(input, Encoding.UTF8) { }
         public BinaryReader(Stream input, Encoding encoding) : base(input, encoding) { }
         public BinaryReader(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen) { }
 
         public long GetNextNull()
         {
-            long lastPosition = BaseStream.Position;
-
-            while (BaseStream.CanRead)
+            lock (_lock)
             {
-                if (ReadByte() == 0)
-                {
-                    long r = BaseStream.Position;
-                    BaseStream.Position = lastPosition;
-                    return r;
-                }
-            }
+                long lastPosition = BaseStream.Position;
 
-            return -1;
+                while (BaseStream.CanRead)
+                {
+                    if (ReadByte() == 0)
+                    {
+                        long r = BaseStream.Position;
+                        BaseStream.Position = lastPosition;
+                        return r;
+                    }
+                }
+
+                return -1;
+            }
         }
 
         public byte[] ReadByteString()
         {
-            var size = GetNextNull() - BaseStream.Position;
-            return ReadBytes((int)size)[..^1];
+            lock (_lock)
+            {
+                var size = GetNextNull() - BaseStream.Position;
+                return ReadBytes((int)size)[..^1];
+            }
         }
 
         public override string ReadString()
         {
-            string str = "";
-            char chr;
-            while ((int)(chr = ReadChar()) != 0)
-                str += chr;
-            return str;
+            lock (_lock)
+            {
+                string str = "";
+                char chr;
+                while ((int)(chr = ReadChar()) != 0)
+                    str += chr;
+                return str;
+            }
         }
     }
 }
