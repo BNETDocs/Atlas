b0500d2515552f6eff1236c08f7c3a7ebc89b29c
Carl Bennett <carl@carlbennett.me>
2023-02-22 17:09:01 -0600

    Use ToLower() for case-insensitive check

--- src/Atlasd/Battlenet/Protocols/Game/ChatCommands/ChatCommand.cs
+++ src/Atlasd/Battlenet/Protocols/Game/ChatCommands/ChatCommand.cs
@@ -52,7 +52,7 @@ namespace Atlasd.Battlenet.Protocols.Game
             var stripSize = cmd.Length + (text.Length - cmd.Length > 0 ? 1 : 0);
             var _raw = raw[stripSize..];
 
-            switch (cmd)
+            switch (cmd.ToLower())
             {
                 case "admin":
                     return new AdminCommand(_raw, args);
