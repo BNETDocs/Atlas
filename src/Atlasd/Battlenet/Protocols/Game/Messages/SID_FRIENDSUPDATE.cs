25fccb2c4930a037d0c690c3847e1eec9ca3691f
Carl Bennett <carl@carlbennett.me>
2023-02-24 23:45:07 -0600

    Implement friend lookup code in SID_FRIENDSUPDATE

--- src/Atlasd/Battlenet/Protocols/Game/Messages/SID_FRIENDSUPDATE.cs
+++ src/Atlasd/Battlenet/Protocols/Game/Messages/SID_FRIENDSUPDATE.cs
@@ -1,6 +1,7 @@
 ﻿using Atlasd.Battlenet.Exceptions;
 using Atlasd.Daemon;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Atlasd.Battlenet.Protocols.Game.Messages
@@ -22,40 +23,71 @@ namespace Atlasd.Battlenet.Protocols.Game.Messages
 
         public override bool Invoke(MessageContext context)
         {
-            if (context.Direction != MessageDirection.ServerToClient)
-                throw new GameProtocolViolationException(context.Client, $"{MessageName(Id)} must be sent from server to client");
+            if (context == null || context.Client == null || !context.Client.Connected || context.Client.GameState == null) return false;
 
-            /**
-             *  (UINT8) Entry number
-             *  (UINT8) Status
-             *  (UINT8) Location id
-             * (UINT32) Product id
-             * (STRING) Location name
-             */
+            switch (context.Direction)
+            {
+                case MessageDirection.ClientToServer:
+                {
+                    /**
+                     * (UINT8) Entry number
+                     */
 
-            var entry = (byte)context.Arguments["entry"];
-            var friend = (Friend)context.Arguments["friend"];
-            var status = (byte)friend.StatusId;
-            var location = (byte)friend.LocationId;
-            var product = (UInt32)friend.ProductCode;
-            var locationStr = (byte[])friend.LocationString;
+                    if (Buffer.Length != 1) throw new GameProtocolViolationException(context.Client, $"{MessageName(Id)} buffer must be exactly 1 byte");
 
-            var bufferSize = (uint)(8 + locationStr.Length);
+                    using var m = new MemoryStream(Buffer);
+                    using var r = new BinaryReader(m);
 
-            Buffer = new byte[bufferSize];
+                    var entry = r.ReadByte();
 
-            using var m = new MemoryStream(Buffer);
-            using var w = new BinaryWriter(m);
+                    var friendStrings = (List<byte[]>)context.Client.GameState.ActiveAccount.Get(Account.FriendsKey, new List<byte[]>());
+                    var friendString = friendStrings[entry];
+                    var friend = new Friend(context.Client.GameState, friendString);
+                    friend.Sync(context.Client.GameState);
 
-            w.Write(entry);
-            w.Write(status);
-            w.Write(location);
-            w.Write(product);
-            w.WriteByteString(locationStr);
+                    return new SID_FRIENDSUPDATE().Invoke(new MessageContext(context.Client, MessageDirection.ServerToClient, new Dictionary<string, dynamic>{
+                        { "entry", entry }, { "friend", friend }
+                    }));
+                }
+                case MessageDirection.ServerToClient:
+                {
+                    /**
+                     *  (UINT8) Entry number
+                     *  (UINT8) Status
+                     *  (UINT8) Location id
+                     * (UINT32) Product id
+                     * (STRING) Location name
+                     */
 
-            Logging.WriteLine(Logging.LogLevel.Debug, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"[{Common.DirectionToString(context.Direction)}] {MessageName(Id)} ({4 + Buffer.Length} bytes)");
-            context.Client.Send(ToByteArray(context.Client.ProtocolType));
-            return true;
+                    var entry = (byte)context.Arguments["entry"];
+                    var friend = (Friend)context.Arguments["friend"];
+                    var status = (byte)friend.StatusId;
+                    var location = (byte)friend.LocationId;
+                    var product = (UInt32)friend.ProductCode;
+                    var locationStr = (byte[])friend.LocationString;
+
+                    var bufferSize = (uint)(8 + locationStr.Length);
+
+                    Buffer = new byte[bufferSize];
+
+                    using var m = new MemoryStream(Buffer);
+                    using var w = new BinaryWriter(m);
+
+                    w.Write(entry);
+                    w.Write(status);
+                    w.Write(location);
+                    w.Write(product);
+                    w.WriteByteString(locationStr);
+
+                    Logging.WriteLine(Logging.LogLevel.Debug, Logging.LogType.Client_Game, context.Client.RemoteEndPoint, $"[{Common.DirectionToString(context.Direction)}] {MessageName(Id)} ({4 + Buffer.Length} bytes)");
+                    context.Client.Send(ToByteArray(context.Client.ProtocolType));
+                    return true;
+                }
+                default:
+                {
+                    throw new GameProtocolException(context.Client);
+                }
+            }
         }
     }
 }
