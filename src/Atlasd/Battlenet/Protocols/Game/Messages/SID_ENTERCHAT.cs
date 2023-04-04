45839931bb14ded1fbcb5cb1d4ba5895040d7923
Caaaaarrrrlll <carl@bnetdocs.org>
2023-04-03 19:44:04 -0500

    Alphabetize namespace includes

--- src/Atlasd/Battlenet/Protocols/Game/Messages/SID_ENTERCHAT.cs
+++ src/Atlasd/Battlenet/Protocols/Game/Messages/SID_ENTERCHAT.cs
@@ -1,9 +1,9 @@
 ﻿using Atlasd.Battlenet.Exceptions;
 using Atlasd.Daemon;
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Atlasd.Battlenet.Protocols.Game.Messages
